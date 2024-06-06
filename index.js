5f4d9fadf4682135ce722f49533c3401a2692a3f
Ly My Y <lymy7724@gmail.com>
2024-06-06 13:05:21 -0400

    Update index.js
    added asynchronous operations to simulate delays

--- index.js
+++ index.js
@@ -25,10 +25,12 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 
+// displaying all the restaurants in the array
 app.get("/api/data", (req, res) => {
   res.json(restaurants);
 });
 
+// adding a new restaurant to the array
 app.post("/api/data", (req, res) => {
   const restaurant = {
     id: restaurants.length + 1,
@@ -36,9 +38,12 @@ app.post("/api/data", (req, res) => {
     type: req.body.type,
   };
   restaurants.push(restaurant);
-  res.send(restaurant);
+  setTimeout(() => {
+    res.send(restaurant);
+  }, 3000);
 });
 
+// updating an existing restuarant in the array
 app.put("/api/data/:id", (req, res) => {
   const restaurant = restaurants.find((c) => c.id === parseInt(req.params.id));
   if (!restaurant) return res.status(404).send("This restaurant doesn't exist");
@@ -47,7 +52,10 @@ app.put("/api/data/:id", (req, res) => {
     return obj.id === parseInt(req.params.id);
   });
   restaurants[index] = req.body;
-  res.send(restaurants);
+
+  setTimeout(() => {
+    res.send(restaurant);
+  }, 3000);
 });
 
 app.delete("/api/data/:id", (req, res) => {
@@ -57,7 +65,9 @@ app.delete("/api/data/:id", (req, res) => {
   const index = restaurants.indexOf(restaurant);
   restaurants.splice(index, 1);
   // return the same course
-  res.send(restaurant);
+  setTimeout(() => {
+    res.send(restaurant);
+  }, 3000);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
